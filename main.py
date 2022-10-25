52cc7c9cd4fab23099dbbbee43a0ed9d36260407
LucaWilliams4831 <lucaw4831@gmail.com>
2022-10-25 03:56:46 +0300

    second commit

--- main.py
+++ main.py
@@ -76,7 +76,7 @@ class CoinClient(RestClient):
 
         balance = self.account_resource(
             account_address,
-            f"0x1::coin::CoinStore<{coin_address}::moon_coin::MoonCoin>",
+            f"0x1::coin::CoinStore<{coin_address}>",
         )
         return balance["data"]["coin"]["value"]
 
@@ -89,7 +89,7 @@ class CoinClient(RestClient):
 
         data = self.account_resource(
             account_address,
-            f"0x1::coin::CoinStore<{coin_address}::moon_coin::MoonCoin>",
+            f"0x1::coin::CoinStore<{coin_address}::tesla_token::TeslaToken>",
         )
         return data
 
@@ -103,10 +103,11 @@ if __name__ == "__main__":
 
     print("h")
     rest_client = CoinClient("https://fullnode.mainnet.aptoslabs.com/v1")
-    r = rest_client.get_reserves("0x05a97986a9d031c4567e15b797be516910cfcb4156312482efc6a19c0a30c948", "0x05a97986a9d031c4567e15b797be516910cfcb4156312482efc6a19c0a30c948")
-
-    #TOKEN = "5529214043:AAGGnFv6MZPE5-pFcaElPapazNY4_GHlUu8"
-    #chat_id = -618973730
-    #message = "hello from your telegram bot"
-    #url = f"https://api.telegram.org/bot{TOKEN}/sendMessage?chat_id={chat_id}&text={message}"
+    # r = rest_client.get_balance("0x5096d4314db80c0fde2a20ffacec0093e41ce6517bbe11cb9572af2bd8ef0303", "0x6c8f6a9c2b66a2590b68c870bb61dd2fdab6d30bed7bc2e7cf7bd59265f04301")
+    r = rest_client.get_balance("0x190d44266241744264b964a37b8f09863167a12d3e70cda39376cfb4e3561e12", "0x05a97986a9d031c4567e15b797be516910cfcb4156312482efc6a19c0a30c948")
+    print(r)    
+    TOKEN = "5529214043:AAGGnFv6MZPE5-pFcaElPapazNY4_GHlUu8"
+    chat_id = -618973730
+    message = "hello from your telegram bot from luca"
+    # url = f"https://api.telegram.org/bot{TOKEN}/sendMessage?chat_id={chat_id}&text={message}"
     # print(requests.get(url).json()) # this sends the message
